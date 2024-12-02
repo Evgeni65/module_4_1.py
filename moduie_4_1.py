292707feafd08cad7f927a0ec8000f1a2d9a00e7
Evgeni65 <evgenikorolev56@gmail.com>
2024-12-02 20:12:45 +0300

    Add files via upload

---
+++ moduie_4_1.py
@@ -0,0 +1,6 @@
+import fake_math as fm
+import true_math as tm
+fm.divide(56,8 )
+fm.divide(56,0 )
+tm.divide(27,3 )
+tm.divide(27,0 )
---
+++ true_math.py
@@ -0,0 +1,7 @@
+def divide(first,second ):
+    if second == 0:
+        print(float('inf'))
+        return
+    else:
+        print(first / second)
+
